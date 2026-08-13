7efe5c57a12e5cc300ba460c9aaddcd7764de982
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-13 16:41:58 +0600

    some that is check element in array

---
+++ prac.js
@@ -0,0 +1,20 @@
+// let numbers=[12,25,8,40,18];
+// let result=numbers.find(number => number>20);
+// console.log(result)
+
+
+// let numbers = [12, 18, 27, 35, 42, 20];
+// let result=numbers.find(number=> number>30);
+// // console.log(result)
+
+let numbers = [5, 12, 18, 25, 30, 35, 42, 8];
+let result=numbers.find(number=> number>20 && number%2 === 0);
+// console.log(result)
+
+let values = [5, 12, 18, 25, 30, 35, 42, 8];
+let findAble=values.find(number=> number>10 && number<40 && number % 2 === 0);
+// console.log(findAble)
+
+let prices = [120, 250, 80, 450, 300];
+let check=prices.some(price=> price>400);
+console.log(check)
