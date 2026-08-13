16d78b4acf2a1696d5c2d6c4ab34275cb73d98cf
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-13 09:14:04 +0600

    boolean check

--- even.js
+++ even.js
@@ -1,4 +1,24 @@
 function isEven(number){
     console.log(number%2)
 }
-isEven(48)
+// isEven(48)
+
+function smalliFy(name){
+    const fullname= name +' '+ 'chowdury';
+    return fullname;
+}
+const robinChow= smalliFy("Robin");
+// console.log(robinChow)
+
+function doubleOrHalf(price,isDouble){
+    if(isDouble===true){
+    const askingPrice=price*2;
+        return askingPrice;
+    }
+    else{
+       const askingPrice=price/2;
+    }
+}
+const myPrice=doubleOrHalf(100, true);
+console.log("price for mango",myPrice)
+console.log("price for leader",myPrice)
