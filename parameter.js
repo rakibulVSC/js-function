643eb8e2673a06c4fb29abcf26c7c6d88c4257b3
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-13 08:36:05 +0600

    multiply number

--- parameter.js
+++ parameter.js
@@ -1,4 +1,18 @@
 function sayHello(name){
     console.log('Hello!',name)
 }
-sayHello('Rahim')
+// sayHello('Rahim')
+
+
+function add(num1,num2){
+console.log('You have provided:',num1,num2)
+const total=num1+num2;
+console.log("total of sum:",total)
+}
+// add(3,7)
+ 
+function multiply(a,b){
+    const result=a*b;
+    console.log("Multiply result is:",result)
+}
+multiply(6,6)
